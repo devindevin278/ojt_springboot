5ce195a349b9e1f1a7e899dfa9bc6ba8ad7ace2c
devindevin278 <130270465+devindevin278@users.noreply.github.com>
2023-08-24 18:20:47 +0700

    commit1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication
+@EntityScan("com.example.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
