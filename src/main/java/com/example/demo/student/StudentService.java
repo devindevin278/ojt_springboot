5ca116c32d56c02ab711a7a4a25ab07c1376bb0f
devindevin278 <130270465+devindevin278@users.noreply.github.com>
2023-08-25 17:50:03 +0700

    create
    coba bikin create student ke database, bikin view register pakai thymeleaf

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,6 +22,14 @@ public class StudentService {
     
     public List<Student> getStudents() {
 		return studentRepository.findAll();
+	}
 
+	public void addNewStudent(Student student) {
+		Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+		if(studentByEmail.isPresent()) {
+			throw new IllegalStateException("email taken");
+		}
+
+		studentRepository.save(student);
 	}
 }
